67b13331b8966740cab9a66c2c3267ccda7ebf6b
nicholas <nab8kd@virginia.edu>
2014-04-16 14:10:43 -0400

    mouse mover sends duration of animation

--- socketapp/socketclient.js
+++ socketapp/socketclient.js
@@ -7,24 +7,26 @@ socket.on('assign-id', function(data) {
 });
 
 var mouseCounter = 0;
+var prevTime = Date.now();
+var duration, currTime;
 $('body').mousemove(function(event) {
 	if(mouseCounter++ > 1) {
-		socket.emit('clientMousemove', { 'clientID': clientID, mouseX: event.pageX, mouseY: event.pageY});
+		currTime - Date.now();
+		duration = currTime - prevTime;
+		prevTime = currTime;
+		socket.emit('clientMousemove', {
+			'clientID': clientID,
+			mouseX: event.pageX,
+			mouseY: event.pageY,
+			'duration': duration
+		});
 		mouseCounter = 0;
 	}
 });
 
-var duration = 10;
-function setAnimateDuration(dur) {
-	duration = dur;
-}
-
-var prevX, prevY, diffX, diffY, distance;
 socket.on('serverMousemove', function(data) {
 	console.log(data);
-	diffX = data.mouseX - prevX;
-	diffY = data.mouseY - prevY;
-	distance = Math.sqrt(diffX*diffX + diffY*diffY);
-	duration = 5 + distance / 30;
-	$('#cursor').animate({'left': data.mouseX, 'top': data.mouseY}, duration, 'linear');
+	if(data.clientID != clientID) {//data.clientID / 2 == clientID / 2
+		$('#cursor').animate({'left': data.mouseX, 'top': data.mouseY}, data.duration, 'linear');
+	}
 });
